0a0b45e72ca759bc6af5d35055088401d2ac4ac3
Hero Bird <robbepop@web.de>
2017-09-13 01:14:58 +0200

    renamed OutputBuffer to SignalBuffer

--- src/layer/signal_buffer.rs
+++ src/layer/signal_buffer.rs
@@ -3,15 +3,15 @@ use ndarray::prelude::*;
 use errors::{Error, Result};
 
 #[derive(Debug, Clone, PartialEq)]
-pub struct OutputBuffer(Array1<f32>);
+pub struct SignalBuffer(Array1<f32>);
 
-impl OutputBuffer {
-	pub fn new(len: usize) -> Result<OutputBuffer> {
+impl SignalBuffer {
+	pub fn new(len: usize) -> Result<SignalBuffer> {
 		use std::iter;
 		if len == 0 {
 			return Err(Error::zero_sized_output_buffer())
 		}
-		Ok(OutputBuffer(
+		Ok(SignalBuffer(
 			Array::from_iter(iter::repeat(0.0)
 				.take(len)
 				.chain(iter::once(1.0))
@@ -19,10 +19,10 @@ impl OutputBuffer {
 		))
 	}
 
-	pub fn from_slice(values: ArrayView1<f32>) -> Result<OutputBuffer> {
+	pub fn from_slice(values: ArrayView1<f32>) -> Result<SignalBuffer> {
 		let mut buf = Array::zeros(values.dim() + 1);
 		buf.assign(&values);
-		Ok(OutputBuffer(buf))
+		Ok(SignalBuffer(buf))
 	}
 
 	pub fn biased_view(&self) -> ArrayView1<f32> {
