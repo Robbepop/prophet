7e7075bdd71e2077ebf2196882de7770020e4f52
Hero Bird <robbepop@web.de>
2017-09-16 23:05:30 +0200

    implemented some of the layer specific traits for ActivationLayer and FullyConnectedLayer

--- src/layer/activation_layer.rs
+++ src/layer/activation_layer.rs
@@ -1,8 +1,16 @@
 use layer::signal_buffer::SignalBuffer;
 use layer::gradient_buffer::GradientBuffer;
+use layer::traits::{
+	ProcessSignal,
+	CalculateErrorGradients,
+	HasOutputSignal,
+	HasGradientBuffer,
+	PropagateGradients
+};
 use errors::{Result};
 use activation::Activation;
 
+/// Activation layers simply apply their activation function onto incoming signals.
 #[derive(Debug, Clone, PartialEq)]
 pub struct ActivationLayer {
 	outputs  : SignalBuffer,
@@ -11,11 +19,74 @@ pub struct ActivationLayer {
 }
 
 impl ActivationLayer {
+	/// Creates a new `ActivationLayer` with the given number of inputs
+	/// and outputs (not respecting the bias input and output) and given
+	/// an activation function.
 	pub fn with_activation(len: usize, act: Activation) -> Result<Self> {
 		Ok(ActivationLayer{
 			outputs  : SignalBuffer::zeros(len)?,
 			gradients: GradientBuffer::zeros(len)?,
 			act
 		})
+	}
+
+	pub fn len(&self) -> usize {
+		self.outputs.view().dim()
+	}
+}
+
+impl ProcessSignal for ActivationLayer {
+	fn process_signal(&mut self, signal: &SignalBuffer) {
+		if self.output_signal().len() != signal.len() {
+			panic!("Error: unmatching signals to layer size") // TODO: Replace this with error. (Needs to change trait.) 
+		}
+		let act = self.act; // Required since borrow-checker doesn't allow
+		                    // using `self.act` within method-call context.
+		self.outputs.view_mut().mapv_inplace(|o| act.base(o))
+	}
+}
+
+impl CalculateErrorGradients for ActivationLayer {
+	fn calculate_gradient_descent(&mut self, target_signals: &SignalBuffer) {
+		use ndarray::Zip;
+
+		debug_assert_eq!(self.output_signal().biased_len(), target_signals.len()); // No calculation for bias neurons.
+
+		let act = self.act; // Required because of non-lexical borrows.
+		Zip::from(&mut self.gradients.view_mut())
+			.and(&self.outputs.view())
+			.and(&target_signals.view())
+			.apply(|g, &t, &o| {
+				*g = (t - o) * act.derived(o)
+			}
+		);
+	}
+}
+
+impl PropagateGradients for ActivationLayer {
+	fn propagate_gradients<P>(&mut self, propagator: P)
+		where P: HasGradientBuffer
+	{
+		unimplemented!()
+	}
+}
+
+impl HasOutputSignal for ActivationLayer {
+	fn output_signal(&self) -> &SignalBuffer {
+		&self.outputs
+	}
+
+	fn output_signal_mut(&mut self) -> &mut SignalBuffer {
+		&mut self.outputs
+	}
+}
+
+impl HasGradientBuffer for ActivationLayer {
+	fn gradients(&self) -> &GradientBuffer {
+		&self.gradients
+	}
+
+	fn gradients_mut(&mut self) -> &mut GradientBuffer {
+		&mut self.gradients
 	}
 }
--- src/layer/fully_connected_layer.rs
+++ src/layer/fully_connected_layer.rs
@@ -1,6 +1,12 @@
 use layer::signal_buffer::SignalBuffer;
 use layer::gradient_buffer::GradientBuffer;
 use layer::weights_matrix::WeightsMatrix;
+use layer::traits::{
+	ProcessSignal,
+	CalculateErrorGradients,
+	HasOutputSignal,
+	HasGradientBuffer
+};
 use errors::{Result};
 
 #[derive(Debug, Clone, PartialEq)]
@@ -16,18 +22,60 @@ impl FullyConnectedLayer {
 		let (inputs, outputs) = (weights.inputs(), weights.outputs());
 		Ok(FullyConnectedLayer{
 			weights,
-			deltas: WeightsMatrix::zeros(inputs, outputs)?,
-			outputs: SignalBuffer::zeros(outputs)?,
+			deltas   : WeightsMatrix::zeros(inputs, outputs)?,
+			outputs  : SignalBuffer::zeros(outputs)?,
 			gradients: GradientBuffer::zeros(outputs)?,
 		})
 	}
 
 	pub fn random(inputs: usize, outputs: usize) -> Result<Self> {
-		Ok(FullyConnectedLayer{
-			weights: WeightsMatrix::random(inputs, outputs)?,
-			deltas: WeightsMatrix::zeros(inputs, outputs)?,
-			outputs: SignalBuffer::zeros(outputs)?,
-			gradients: GradientBuffer::zeros(outputs)?,
-		})
+		Ok(FullyConnectedLayer::with_weights(
+			WeightsMatrix::random(inputs, outputs)?)?)
+	}
+}
+
+impl ProcessSignal for FullyConnectedLayer {
+	fn process_signal(&mut self, signal: &SignalBuffer) {
+		if self.output_signal().len() != signal.len() {
+			panic!("Error: unmatching signals to layer size") // TODO: Replace this with error. (Needs to change trait.) 
+		}
+		use ndarray::linalg::general_mat_vec_mul;
+		general_mat_vec_mul(1.0, &self.weights.view(), &signal.biased_view(), 1.0, &mut self.outputs.view_mut())
+	}
+}
+
+impl CalculateErrorGradients for FullyConnectedLayer {
+	fn calculate_gradient_descent(&mut self, target_signal: &SignalBuffer) {
+		if self.output_signal().len() != target_signal.len() {
+			panic!("Error: unmatching signals to layer size") // TODO: Replace this with error. (Needs to change trait.) 
+		}
+		use ndarray::Zip;
+		Zip::from(&mut self.gradients.view_mut())
+			.and(&self.outputs.view())
+			.and(&target_signal.view())
+			.apply(|g, &t, &o| {
+				*g = t - o
+			}
+		);
+	}
+}
+
+impl HasOutputSignal for FullyConnectedLayer {
+	fn output_signal(&self) -> &SignalBuffer {
+		&self.outputs
+	}
+
+	fn output_signal_mut(&mut self) -> &mut SignalBuffer {
+		&mut self.outputs
+	}
+}
+
+impl HasGradientBuffer for FullyConnectedLayer {
+	fn gradients(&self) -> &GradientBuffer {
+		&self.gradients
+	}
+
+	fn gradients_mut(&mut self) -> &mut GradientBuffer {
+		&mut self.gradients
 	}
 }
