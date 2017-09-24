c9e163649ce5b675e37cc6e85bdcf32a34ee622c
Hero Bird <robbepop@web.de>
2017-09-24 15:17:10 +0200

    removed commented-out export code that is deprecated since prelude export of traits

--- src/layer/mod.rs
+++ src/layer/mod.rs
@@ -9,12 +9,3 @@ mod container_layer;
 pub(crate) use self::fully_connected_layer::FullyConnectedLayer;
 pub(crate) use self::activation_layer::ActivationLayer;
 pub(crate) use self::container_layer::ContainerLayer;
-
-// pub(crate) use self::traits::{
-// 	ProcessInputSignal,
-// 	HasOutputSignal,
-// 	CalculateOutputErrorSignal,
-// 	HasErrorSignal,
-// 	PropagateErrorSignal,
-// 	ApplyErrorSignalCorrection
-// };
