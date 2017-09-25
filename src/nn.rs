7f7c418b49ef72771131d49e92a2772d2260f4fa
Hero Bird <robbepop@web.de>
2017-09-26 01:51:58 +0200

    added nn.rs module with stub bodies for new neural network impl

---
+++ src/nn.rs
@@ -0,0 +1,48 @@
+use ndarray::ArrayView1;
+
+use layer::utils::{SignalBuffer};
+use layer::{ContainerLayer};
+use traits::prelude::*;
+use utils::{LearnRate, LearnMomentum};
+
+#[derive(Debug, Clone, PartialEq)]
+struct NeuralNet {
+	input: SignalBuffer,
+	layers: ContainerLayer
+}
+
+impl<'a, I> Predict<I> for NeuralNet
+	where I: Into<ArrayView1<'a, f32>>
+{
+	/// Implementation for inputs that do not respect a bias value.
+	fn predict(&mut self, _input: I) -> ArrayView1<f32> {
+		unimplemented!()
+	}
+}
+
+impl<'a> Predict<&'a SignalBuffer> for NeuralNet {
+	/// Implementation for inputs that do respect a bias value.
+	fn predict(&mut self, _input: &SignalBuffer) -> ArrayView1<f32> {
+		unimplemented!()
+	}
+}
+
+impl<'a, A> UpdateGradients<A> for NeuralNet
+	where A: Into<ArrayView1<'a, f32>>
+{
+	/// Implementation for inputs that do not respect a bias value.
+	fn update_gradients(&mut self, _target_values: A) {
+		unimplemented!()
+	}
+}
+
+impl UpdateWeights for NeuralNet
+{
+	fn update_weights(
+		&mut self, 
+		_rate: LearnRate,
+		_momentum: LearnMomentum
+	) {
+		unimplemented!()
+	}
+}
