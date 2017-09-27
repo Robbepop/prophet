7bbb719501a2af0647e859c45fc6b2b7bc3f88f3
Hero Bird <robbepop@web.de>
2017-09-28 01:51:22 +0200

    added public visibility outside of layer::utils sub-module for new ArrayBase sub-types.

--- src/layer/utils/mod.rs
+++ src/layer/utils/mod.rs
@@ -3,6 +3,26 @@ mod matrix_base;
 mod signal_buffer;
 mod buffer_base;
 
-pub(crate) use self::error_signal_buffer::ErrorSignalBuffer;
-pub(crate) use self::matrix_base::{WeightsMatrix, DeltaWeightsMatrix};
 pub(crate) use self::signal_buffer::SignalBuffer;
+pub(crate) use self::error_signal_buffer::ErrorSignalBuffer;
+
+pub(crate) use self::matrix_base::{WeightsMatrix, DeltaWeightsMatrix};
+pub(crate) use self::buffer_base::{
+	BiasedSignalView,
+	UnbiasedSignalView,
+	BiasedErrorSignalView,
+	UnbiasedErrorSignalView,
+
+	BiasedSignalViewMut,
+	UnbiasedSignalViewMut,
+	BiasedErrorSignalViewMut,
+	UnbiasedErrorSignalViewMut,
+
+	BiasedSignalBuffer,
+	UnbiasedSignalBuffer,
+	BiasedErrorSignalBuffer,
+	UnbiasedErrorSignalBuffer,
+
+	Iter,
+	IterMut
+};
