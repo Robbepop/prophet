8764f9922ac383392beada7b99aac6944134fadd
Hero Bird <robbepop@web.de>
2017-10-05 22:22:50 +0200

    made trainer/sample submodule items publicly visible in trainer module

--- src/trainer/mod.rs
+++ src/trainer/mod.rs
@@ -1,6 +1,14 @@
 pub mod condition;
 mod sample;
 mod mentor;
+pub use self::sample::{
+	SupervisedSample,
+	Sample,
+	SampleCollection,
+	SampleGen,
+	SequentialSampleScheduler,
+	RandomSampleScheduler
+};
 
 pub use self::condition::{
 	TrainingState,
