00a272854c402f3a0bb301c510a7b02d27ecc6ca
Hero Bird <robbepop@web.de>
2017-10-05 22:27:55 +0200

    make the new mentoring and trait items of trainer module publicly visible

--- src/trainer/mod.rs
+++ src/trainer/mod.rs
@@ -1,6 +1,8 @@
 pub mod condition;
 mod sample;
 mod mentor;
+mod traits;
+
 pub use self::sample::{
 	SupervisedSample,
 	Sample,
@@ -13,4 +15,15 @@ pub use self::sample::{
 pub use self::condition::{
 	TrainingState,
 	TrainCondition
+};
+
+pub use self::mentor::{
+	Mentor,
+	MentorBuilder,
+	Context
+};
+
+pub use self::traits::{
+	PredictSupervised,
+	OptimizeSupervised
 };
