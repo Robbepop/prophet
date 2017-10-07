a0137de1a379d6c70bc9162f657e26bbc0745340
Hero Bird <robbepop@web.de>
2017-10-07 14:05:53 +0200

    added doc-comments for MeanSquaredError

--- src/trainer/utils.rs
+++ src/trainer/utils.rs
@@ -2,10 +2,23 @@ use ndarray::AsArray;
 
 use errors::{Result};
 
+/// Represents a mean-squared-error value.
+/// 
+/// Note that a mean-squared-error can never be a negative value.
+/// This is used mainly during the training process as a statistics
+/// for supervised learning that may be used to query a state when the
+/// training process can be halted.
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub struct MeanSquaredError(f32);
 
 impl MeanSquaredError {
+	/// Creates a new `MeanSquaredError` from the given `f32` value representing it.
+	/// 
+	/// This is used mainly to initialize `MeanSquaredError` types.
+	/// 
+	/// # Errors
+	/// 
+	/// - If the given mse is negative.
 	pub fn new(mse: f32) -> Result<MeanSquaredError> {
 		if mse.is_sign_negative() {
 			// TODO: Handle errors properly.
@@ -14,6 +27,18 @@ impl MeanSquaredError {
 		Ok(MeanSquaredError(mse))
 	}
 
+	/// Calculates a new `MeanSquaredError` from the two given array-like components.
+	/// 
+	/// Note that for performance reasons this does not calculate the real mean-squared-error
+	/// as a sum of the squared differences divided by the number of elements but instead only
+	/// divides by two `2` to make its deviation simpler and thus improve the overall performance.
+	/// Read more here: [Link](https://de.wikipedia.org/wiki/Backpropagation#Fehlerminimierung)
+	/// 
+	/// # Errors
+	/// 
+	/// - If the array representing the actual values has a length of zero (`0`).
+	/// - If the array representing the expected values has a length of zero (`0`).
+	/// - If the given arrays have unmatching lengths.
 	pub fn from_arrays<'a, 'e, A, E>(actual: A, expected: E) -> Result<MeanSquaredError>
 		where A: AsArray<'a, f32>,
 		      E: AsArray<'e, f32>
@@ -42,6 +67,7 @@ impl MeanSquaredError {
 		)
 	}
 
+	/// Returns the `f32` representation of this `MeanSquaredError`.
 	#[inline]
 	pub fn to_f32(self) -> f32 {
 		self.0
